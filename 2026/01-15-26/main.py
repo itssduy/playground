6340a876a2233d47908beaf16155ea86de5498e1
Duy <lolmebunbun@gmail.com>
2026-01-24 02:17:29 -0600

    keeping up to date

---
+++ 2026/01-15-26/main.py
@@ -0,0 +1,9 @@
+def main():
+    printArray([1,2,3,4,5])
+
+def printArray(arr):
+    for i in arr:
+        print(i)
+
+if __name__ == "__main__":
+    main()
---
+++ 2026/01-20-26/main.py
@@ -0,0 +1,4 @@
+test = [1,2,3,4,5,6,7,8,9]
+
+for i in test:
+    print(i)
